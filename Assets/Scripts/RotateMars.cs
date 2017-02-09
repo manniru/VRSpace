df85924b84720fa24f26fe03926aceaf3dca1b97
OA <miohoya@gmail.com>
2017-02-08 21:59:30 -0500

    feat: project setup

---
+++ Assets/Scripts/RotateMars.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMars : MonoBehaviour {
+
+	public GameObject marsParent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		iTween.RotateBy(marsParent, iTween.Hash("y", 0.1f, "time", 10f, "easetype", "linear", "looptype", iTween.LoopType.loop));
+	}
+}
